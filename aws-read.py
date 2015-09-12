3a3c4946cf54ae63c7a51f0c51a78e31383882a5
mkimura3 <mkimura3@msn.com>
2015-09-13 01:54:07 +0900

    ec2インスタンス読み込み機能追加中

--- aws-read.py
+++ aws-read.py
@@ -38,15 +38,104 @@ class AwsTest(unittest.TestCase):
     def test_aws_estimate(self):
         driver = self.driver
         driver.get(self.base_url + ESTIMATE_URL)
-        # 詳細ボタン
+        # 概要の取得
+        solution = driver.find_element_by_css_selector("table.SolutionShowBody")
+        pp( self.get_solution(solution) )
+
+        # 詳細ボタンを押す
         driver.find_element_by_css_selector("table.Buttons > tbody > tr > td:nth-child(3) > button").click()
         # 無料利用枠チェック外す
-        self.disable_freetier() 
+        self.disable_freetier()
+
         # 見積もりを取得する
         bill=driver.find_element_by_css_selector("table.bill") 
-        self.get_estimate(bill)
+        pp ( self.get_estimate(bill) )
+
+        # Serviceメニューの取得
+        self.init_serviceMenu() 
+       
+        # EC2構成情報の取得
+        self.select_service('Amazon EC2') 
+        ec2tbl = driver.find_element_by_css_selector('table.service.EC2Service')
+        self.get_ec2Service(ec2tbl)
 
 
+    def init_serviceMenu(self):
+        self.serviceTab = {}
+        tabs = self.driver.find_elements_by_css_selector("div.servicesPanel > div.tabs > div.tab[aria-hidden=false]")
+        for tab in tabs:
+            self.serviceTab[ tab.text.strip() ] = tab
+
+    def select_service(self, serviceName):
+            self.serviceTab[serviceName].click()
+
+    def get_solution(self,solution):
+        sc_price_txt = solution.find_element_by_css_selector("div.gwt-HTML.SC_Price").text
+        # 金額
+        sc_price = float(sc_price_txt.split(' ')[-1])
+        # 名前
+        sc_name = solution.find_element_by_css_selector("table.SC_SOLUTION_LINE div.SC_SOLUTION_DATA").text
+        # 含まれるもの
+        sc_include = solution.find_element_by_css_selector("table.DescriptiveDetails div.SC_INCLUDES_DATA").text
+        # 説明
+        sc_desc = solution.find_element_by_css_selector("table.DescriptiveDetails div.SC_DESCRIPTION_DATA").text
+        return {
+                'price' : sc_price,
+                'name' : sc_name,
+                'includes' : sc_include,
+                'description' : sc_desc
+            }
+
+    def get_ec2Service(self,table):
+        # EC2タブの選択
+        # インスタンス行取得
+        rows = table.find_elements_by_css_selector('div.Instances table.itemsTable tr.EC2InstanceRow.itemsTableDataRow')
+        for row in rows:
+            # インスタンス説明
+            desc = row.find_element_by_css_selector("table.SF_EC2_INSTANCE_FIELD_DESCRIPTION input").get_attribute('value')
+            # インスタンス数
+            instances = int(row.find_element_by_css_selector("table.SF_EC2_INSTANCE_FIELD_INSTANCES input").get_attribute('value'))
+            # 使用料
+            usage_val = int(row.find_element_by_css_selector("table.SF_EC2_INSTANCE_FIELD_USAGE input.numericTextBox").get_attribute('value'))
+            s = row.find_element_by_css_selector("table.SF_EC2_INSTANCE_FIELD_USAGE select")
+            usage_type=s.find_elements_by_tag_name('option')[ int(s.get_attribute('selectedIndex')) ].text
+            # 選択されている設定を取得
+            pp(self.get_instanceType(row))
+            # 料金計算オプション
+
+
+    def get_instanceType(self,row):
+        driver = self.driver
+        type_div = row.find_element_by_css_selector('div.SF_EC2_INSTANCE_FIELD_TYPE')
+        tlines = type_div.text.splitlines()
+        # OS
+        instance_os = tlines[0].split(u'、')[0].strip()
+        # インスタンスタイプ
+        instance_type = tlines[0].split(u'、')[1].strip()
+        # インスタンスタイプを開く
+        type_div.click()
+        # タイプリストが展開されるまで待つ
+        self.wait.until( expected_conditions.presence_of_element_located((By.CSS_SELECTOR , 'table.Types > tbody > tr:nth-child(2)')) )
+        # EBS最適化
+        optstr = driver.find_element_by_css_selector("table.SF_EC2_INSTANCE_FIELD_EBS_OPTIMIZED input[type='checkbox']").get_attribute('checked')
+        instance_ebsopt = True if optstr==u'true' else False
+        # 詳細モニタリング
+        monstr = driver.find_element_by_css_selector("table.SF_EC2_INSTANCE_FIELD_MONITORING input[type='checkbox']").get_attribute('checked')
+        instance_monitor = True if monstr==u'true' else False
+        # ハードウェア占有
+        tenastr = driver.find_element_by_css_selector("table.SF_EC2_INSTANCE_FIELD_TENANCY input[type='checkbox']").get_attribute('checked')
+        instance_tenancy = True if tenastr==u'true' else False
+        # ダイアログを閉じる
+        driver.find_element_by_css_selector('table.Buttons > tbody > tr > td:nth-child(3) > table > tbody > tr > td:nth-child(3) > button').click()
+        # 
+        return {
+                'os': instance_os,
+                'type' : instance_type,
+                'ebs_optimized' : instance_ebsopt,
+                'detailed_monitor' : instance_monitor,
+                'dedicated' : instance_tenancy
+            }
+         
     def get_estimate(self,bill):
         def is_member(target , cname):
             return cname in target.get_attribute("class").split(" ")
@@ -58,7 +147,7 @@ class AwsTest(unittest.TestCase):
         for btn in btns: 
             btn.click()
         # 月額合計
-        total = bill.find_element_by_css_selector('tr.total:not([aria-hidden]) table.value>tbody>tr>td:nth-child(2)>input').get_attribute('value')
+        total = float(bill.find_element_by_css_selector('tr.total:not([aria-hidden]) table.value>tbody>tr>td:nth-child(2)>input').get_attribute('value'))
         # 展開された見積もりを順に取得
         rows = bill.find_elements_by_css_selector("tr[aria-hidden=false]")
         estimate={
@@ -68,23 +157,34 @@ class AwsTest(unittest.TestCase):
         s={}
         for row in rows:
             if is_member(row,'summary') :
-                if s : estimate['detail'].append(s)
+                if s : 
+                    estimate['detail'].append(s)
                 s={}
                 # label
                 s['name'] = row.find_element_by_css_selector('td:nth-child(2)>div.label').text
                 # subTotal
-                s['subTotal'] = row.find_element_by_css_selector('table.value>tbody>tr>td:nth-child(2)>input').get_attribute('value')
+                s['subTotal'] = float(row.find_element_by_css_selector('table.value>tbody>tr>td:nth-child(2)>input').get_attribute('value'))
                 s['items'] = []
             elif is_member(row,'field'):
                 i = {}
                 # sublabel
                 i['name'] = row.find_element_by_css_selector('td:nth-child(1)>div.label').text
                 # price
-                i['price'] = row.find_element_by_css_selector('table.value>tbody>tr>td:nth-child(2)>input').get_attribute('value')
+                i['price'] = float(row.find_element_by_css_selector('table.value>tbody>tr>td:nth-child(2)>input').get_attribute('value'))
                 s['items'].append(i)
+        # 見積もり合計値のチェック
+        self.check_estimate(estimate)
+        return estimate
 
-        pp(estimate)
-         
+    def check_estimate(self, estimate):
+        # subTotalの合計が月額合計と等しいか
+        assert round(estimate['total'],2)== round(sum([ a['subTotal'] for a in estimate['detail'] ] ),2)
+
+        #各subTotalが項目合計と等しいか
+        for field in estimate['detail']:
+            print field['name'], field['subTotal']  
+            assert round(field['subTotal'],2) == round(sum([ a['price']  for a in field['items'] ]),2)
+                
 
     def disable_freetier(self):
         # 無料利用枠チェック外す
