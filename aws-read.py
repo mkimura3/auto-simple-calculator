d6ac8255212406c23515eb9d2bf7bfe0ed387174
mkimura3 <mkimura3@msn.com>
2015-09-22 20:08:49 +0900

    VPCに対応、JSONでの出力に対応

--- aws-read.py
+++ aws-read.py
@@ -21,7 +21,7 @@ def pp(obj):
         print obj
  
 ESTIMATE_URL='/index.html?lng=ja_JP#r=NRT&s=EC2&key=calc-FreeTier-NGC-140321'
-ESTIMATE_URL='/index.html?lng=ja_JP#r=NRT&s=EC2&key=calc-7DBA9B32-1A4C-4829-AD7C-AE102ED1F10F'
+ESTIMATE_URL='/index.html?lng=ja_JP#r=NRT&s=EC2&key=calc-9C7A8309-7AE5-4FF0-B888-82F15EDDBE68'
 
 class AwsTest(unittest.TestCase):
 
@@ -79,9 +79,14 @@ class AwsTest(unittest.TestCase):
         driver = self.driver
         driver.get(self.base_url + ESTIMATE_URL)
         # 概要の取得
-        solution = self.get_element("table.SolutionShowBody")
-        pp( self.get_solution(solution) )
+        sol = self.get_element("table.SolutionShowBody")
+        solution = {
+            'Solution' : self.get_solution(sol)
+        }
+        #pp(solution)
+        #print '-------------------------------------------'
 
+        #
         # 詳細ボタンを押す
         self.get_element("table.Buttons > tbody > tr > td:nth-child(3) > button").click()
         # 無料利用枠チェック外す
@@ -91,19 +96,33 @@ class AwsTest(unittest.TestCase):
 
         # 見積もりを取得する
         bill=self.get_element("table.bill") 
-        estimate = self.get_estimate(bill)
-        pp(estimate)
+        estimate = {
+            'Estimate' : self.get_estimate(bill)
+        }
+        #pp(estimate)
+        #print '-------------------------------------------'
+
         # Regionリストの取得
         self.init_regionList()
         
         # 見積もり項目ごとに構成を取得する
-        for srvc in estimate['detail']:
-            m = re.match(u'^(.*)Service（(.*)）.*', srvc['name'])
+        systemconf = {}
+        for srvc in estimate['Estimate']['Detail']:
+            m = re.match(u'^(.*)Service（(.*)）.*', srvc['Name'])
             if m  :
-                print m.group(0)
-                sconf = self.get_awsService( m.group(1).strip() , m.group(2).strip() ) 
-                pp(sconf)
-
+                #print m.group(0)
+                sc= self.get_awsService( m.group(1).strip() , m.group(2).strip() ) 
+                #pp(sc)
+                systemconf.update(sc)
+        #
+        #pp(systemconf)
+        
+        solution.update(estimate)
+        solution.update({'SystemConfiguration' : systemconf })
+       
+        print json.dumps(solution, indent=4, ensure_ascii=False ) 
+        #pp(solution)
+        
 
 
     def init_serviceMenu(self):
@@ -158,19 +177,71 @@ class AwsTest(unittest.TestCase):
             # print service_name, region_text
             self.select_service(u'Amazon EC2')
             self.select_region(region_text)
-            return self.get_ec2Service()
+            return { 'Amazon EC2' : self.get_ec2Service() }
         # S3構成情報の取得
         elif ( service_name == 'Amazon S3' ):
             self.select_service(u'Amazon S3')
             self.select_region(region_text)
-            return self.get_s3Service()
+            return { 'Amazon S3': self.get_s3Service() }
         # RDS構成情報の取得
         elif ( service_name == 'Amazon RDS' ):
             self.select_service(u'Amazon RDS')
             self.select_region(region_text)
-            return self.get_rdsService()
+            return { 'Amazon RDS': self.get_rdsService() }
+        # VPC情報の取得
+        elif ( service_name == 'Amazon VPC' ):
+            self.select_service(u'Amazon VPC') 
+            self.select_region(region_text)
+            return { 'Amazon RDS': self.get_vpcService() } 
+        # 未サポート
+        else :
+            return { service_name : 'NotSupportedYet' }
 
-   
+    # -------------------- VPC ----------------------
+    def get_vpcService(self):
+        table = self.get_element('table.service.VPNService')
+        # waitを短く
+        self.driver.implicitly_wait(1)  
+        ####
+        # インスタンス
+        vpccons = []
+        rows = self.get_elements('div.VPNConnections table.itemsTable tr.VPNConnectionRow.itemsTableDataRow', table)
+        for row in rows:
+            # VPC説明
+            desc = self.get_value("table.SF_VPC_FIELD_DESCRIPTION input", row)
+            # 接続数
+            quantity = int(self.get_value("table.SF_VPC_FIELD_INSTANCES input", row))
+            # 使用量
+            usage_val,  usage_type = self.get_val_and_type('table.SF_VPC_FIELD_USAGE', row)
+            # データ転送送信
+            send_data,  send_type = self.get_val_and_type('tr>td.cell:nth-child(5)>table.dataTransferField.amountField', row)
+            # データ転送受信
+            recv_data,  recv_type = self.get_val_and_type('tr>td.cell:nth-child(6)>table.dataTransferField.amountField', row)
+                 
+            vpccon= {
+                'Description' : desc,
+                'Quantity' : quantity,
+                'Usage' : {
+                    'Value' : usage_val,
+                    'Type' : usage_type,
+                },
+                'DataTranfer' : {
+                    'DataCenterSend' : {
+                        'Value' : send_data,
+                        'Type'  : send_type
+                    },
+                    'DataCenterReceive' : {
+                        'Value' : recv_data,
+                        'Type'  : recv_type
+                    }
+                }
+            }
+            vpccons.append(vpccon)
+        return {
+            'VPCConnections' : vpccons
+        }
+
+
     # -------------------- RDS ----------------------
     def get_rdsService(self):
         table = self.get_element('table.service.RDSService')
@@ -201,17 +272,19 @@ class AwsTest(unittest.TestCase):
             iops = int(self.get_value('table.SF_RDS_INSTANCE_FIELD_IOPS input.numericTextBox', row ))
 
             instance= {
-                'description' : desc,
-                'quantity' : quantity,
-                'usageType' : usage_type,
-                'usageValue' : usage_val,
-                'engine' : engine,
-                'type' : instance_type,
-                'deployment' : deploy_type,
-                'storage' : {
-                    'type' : storage_type,
-                    'size' : storage_size,
-                    'iops' : iops
+                'Description' : desc,
+                'Quantity' : quantity,
+                'Usage' : {
+                    'Value' : usage_val,
+                    'Type' : usage_type,
+                },
+                'Engine' : engine,
+                'InstanceType' : instance_type,
+                'Deployment' : deploy_type,
+                'Storage' : {
+                    'Type' : storage_type,
+                    'Size' : storage_size,
+                    'Iops' : iops
                 }
             }
             instances.append(instance)
@@ -221,8 +294,8 @@ class AwsTest(unittest.TestCase):
         for row in rows:
             backup_size , backup_type = self.get_val_and_type('table.SF_RDS_FIELD_BACKUP', row )
             volumes.append({
-                'size' : backup_size,
-                'unit' :  backup_type
+                'Size' : backup_size,
+                'Unit' :  backup_type
             })
 
         # リザーブドインスタンス
@@ -250,27 +323,29 @@ class AwsTest(unittest.TestCase):
             iops = int(self.get_value('table.SF_RDS_INSTANCE_FIELD_IOPS input.numericTextBox', row ))
 
             rinstance= {
-                'description' : desc,
-                'quantity' : quantity,
-                'usageType' : usage_type,
-                'usageValue' : usage_val,
-                'engine' : engine,
-                'type' : instance_type,
-                'deployment' : deploy_type,
-                'storage' : {
-                    'type' : storage_type,
-                    'size' : storage_size,
-                    'iops' : iops
+                'Description' : desc,
+                'Quantity' : quantity,
+                'Usage' : {
+                    'Value' : usage_val,
+                    'Type' : usage_type,
                 },
-                'offeringType' : offering_type,
-                'offeringTerm' : offering_term
+                'Engine' : engine,
+                'Type' : instance_type,
+                'Deployment' : deploy_type,
+                'Storage' : {
+                    'Type' : storage_type,
+                    'Size' : storage_size,
+                    'Iops' : iops
+                },
+                'OfferingType' : offering_type,
+                'OfferingTerm' : offering_term
             }
             rinstances.append(rinstance)
 
         return {
-            'instances' : instances,
-            'backupVolumes' : volumes,
-            'reservedInstances' : rinstances
+            'Instances' : instances,
+            'BackupVolumes' : volumes,
+            'ReservedInstances' : rinstances
         }
 
     # --------------------- S3 ----------------------
@@ -295,27 +370,27 @@ class AwsTest(unittest.TestCase):
         internet_in , internet_in_type = self.get_val_and_type("table.subSection:nth-child(3) div.subContent > table:nth-child(3)", table)
 
         return {
-            'standardStorage': {
-                'size' : s3_size,
-                'unit' : s3_size_unit
+            'StandardStorage': {
+                'Size' : s3_size,
+                'Unit' : s3_size_unit
             },
-            'reducedRedundancy': {
-                'size' : rr_size,
-                'unit' : rr_size_unit
+            'ReducedRedundancy': {
+                'Size' : rr_size,
+                'Unit' : rr_size_unit
             },
-            'putCopyPostListRequests' : req_put,
-            'getOtherRequests' : req_get,
-            "interRegion" : {
-                "value" : inter_region,
-                "type"  : inter_region_type
+            'PutCopyPostListRequests' : req_put,
+            'GetOtherRequests' : req_get,
+            "InterRegion" : {
+                "Value" : inter_region,
+                "Type"  : inter_region_type
             },
-            "internetSend" : {
-                "value" : internet_out,
-                "type"  : internet_out_type
+            "InternetSend" : {
+                "Value" : internet_out,
+                "Type"  : internet_out_type
             },
-            "internetRecive" : {
-                "value" : internet_in,
-                "type"  : internet_in_type
+            "InternetReceive" : {
+                "Value" : internet_in,
+                "Type"  : internet_in_type
             }
         }
 
@@ -343,11 +418,13 @@ class AwsTest(unittest.TestCase):
             instance = self.get_instanceType(row)
             #
             instance.update( {
-                    'description' : desc,
-                    'quantity' : quantity,
-                    'usageType' : usage_type,
-                    'usageValue' : usage_val,
-                    'billingOption' : billing
+                    'Description' : desc,
+                    'Quantity' : quantity,
+                    'Usage' : {
+                        'Value' : usage_val,
+                        'Type' : usage_type,
+                    },
+                    'BillingOption' : billing
                 })
             instances.append(instance)
         ####
@@ -371,13 +448,13 @@ class AwsTest(unittest.TestCase):
             snap_type = self.get_selectedText(s)
             snap_size = float( self.get_value("tr td:nth-child(7) input.gwt-TextBox.numericTextBox",row) )
             storages.append({
-                'description' : desc,
-                'quantity' : quantity,
-                'size' : size,
-                'iops' : iops,
-                'type' : ebs_type,
-                'snapshotType' : snap_type,
-                'snapshotSize' : snap_size
+                'Description' : desc,
+                'Quantity' : quantity,
+                'Size' : size,
+                'Iops' : iops,
+                'EBSType' : ebs_type,
+                'SnapshotType' : snap_type,
+                'SnapshotSize' : snap_size
             })
 
         # Elastic IP
@@ -411,46 +488,46 @@ class AwsTest(unittest.TestCase):
         self.driver.implicitly_wait(15)
 
         return {
-            "instances" : instances,
-            "storages"  : storages,
-            "elasticIp" : {
-                "quantity" : eip_quantity,
-                "unAttached" : {
-                    "value" : eip_notime, 
-                    "type" : eip_notype
+            "Instances" : instances,
+            "Storages"  : storages,
+            "ElasticIp" : {
+                "Quantity" : eip_quantity,
+                "UnAttached" : {
+                    "Value" : eip_notime, 
+                    "Type" : eip_notype
                 }
             },
-            "dataTranfer" : {
-                "interRegion" : {
-                    "value" : interr_data,
-                    "type"  : interr_type
+            "DataTranfer" : {
+                "InterRegion" : {
+                    "Value" : interr_data,
+                    "Type"  : interr_type
                 },
-                "intraRegion" : {
-                    "value" : intra_region,
-                    "type"  : intra_region_type
+                "IntraRegion" : {
+                    "Value" : intra_region,
+                    "Type"  : intra_region_type
                 },
-                "internetSend" : {
-                    "value" : internet_out,
-                    "type"  : internet_out_type
+                "InternetSend" : {
+                    "Value" : internet_out,
+                    "Type"  : internet_out_type
                 },
-                "internetRecive" : {
-                    "value" : internet_in,
-                    "type"  : internet_in_type
+                "InternetReceive" : {
+                    "Value" : internet_in,
+                    "Type"  : internet_in_type
                 },
-                "vpnPeers" : {
-                    "value" : vpcpeer_data,
-                    "type"  : vpcpeer_data_type
+                "VpnPeers" : {
+                    "Value" : vpcpeer_data,
+                    "Type"  : vpcpeer_data_type
                 },
-                "intraRegionEipElb" : {
-                    "value" : eip_data,
-                    "type"  : eip_data_type
+                "IntraRegionEIPELB" : {
+                    "Value" : eip_data,
+                    "Type"  : eip_data_type
                 }
             },
-            "elasticLoadBalancing" : {
-                "quantity" : elb_quantity,
-                "elbTransfer" : {
-                    "value" : elb_total,
-                    "type"  : elb_total_type
+            "ElasticLoadBalancing" : {
+                "Quantity" : elb_quantity,
+                "ELBTransfer" : {
+                    "Value" : elb_total,
+                    "Type"  : elb_total_type
                 }
             }
         }
@@ -479,11 +556,11 @@ class AwsTest(unittest.TestCase):
         self.get_element('table.Buttons > tbody > tr > td:nth-child(3) > table > tbody > tr > td:nth-child(3) > button').click()
         # 
         return {
-                'os': instance_os,
-                'type' : instance_type,
-                'ebsOptimized' : instance_ebsopt,
-                'detailedMonitor' : instance_monitor,
-                'dedicated' : instance_tenancy
+                'OS': instance_os,
+                'InstanceType' : instance_type,
+                'EbsOptimized' : instance_ebsopt,
+                'DetailedMonitor' : instance_monitor,
+                'Dedicated' : instance_tenancy
             }
          
     def is_member(self, target , cname):
@@ -500,8 +577,8 @@ class AwsTest(unittest.TestCase):
         total_items =[] 
         total = float(self.get_value('tr.total:not([aria-hidden]) table.value>tbody>tr>td:nth-child(2)>input',bill))
         total_items.append({
-            'name' : self.get_text('tr.total:not([aria-hidden])>td:nth-child(1)>div.gwt-HTML.label'),
-            'price' : total
+            'Name' : self.get_text('tr.total:not([aria-hidden])>td:nth-child(1)>div.gwt-HTML.label'),
+            'Price' : total
         })
         # サポートやリザーブ分を足す
         totals = self.get_elements('tr.total[aria-hidden=false]',bill)
@@ -509,48 +586,48 @@ class AwsTest(unittest.TestCase):
             #print self.get_text('tr>td>div.gwt-HTML.label',t), self.get_value('table.value>tbody>tr>td:nth-child(2)>input',t)
             v = float(self.get_value('table.value>tbody>tr>td:nth-child(2)>input',t))
             total_items.append({
-                'name' : self.get_text('tr>td>div.gwt-HTML.label',t),
-                'price' : v
+                'Name' : self.get_text('tr>td>div.gwt-HTML.label',t),
+                'Price' : v
             })
             total += v
 
         # 展開された見積もりを順に取得
         rows = self.get_elements("tr[aria-hidden=false]",bill)
         estimate={
-            'total' : total,
-            'totalItems' : total_items,
-            'detail' : []
+            'Total' : total,
+            'TotalItems' : total_items,
+            'Detail' : []
         }
         s={}
         for row in rows:
             if self.is_member(row,'summary') :
                 if s : 
-                    estimate['detail'].append(s)
+                    estimate['Detail'].append(s)
                 s={}
                 # label
-                s['name'] = self.get_text('td:nth-child(2)>div.label', row)
+                s['Name'] = self.get_text('td:nth-child(2)>div.label', row)
                 # subTotal
-                s['subTotal'] = float(self.get_value('table.value>tbody>tr>td:nth-child(2)>input', row))
-                s['items'] = []
+                s['SubTotal'] = float(self.get_value('table.value>tbody>tr>td:nth-child(2)>input', row))
+                s['Items'] = []
             elif self.is_member(row,'field'):
                 i = {}
                 # sublabel
-                i['name'] = self.get_text('td:nth-child(1)>div.label', row)
+                i['Name'] = self.get_text('td:nth-child(1)>div.label', row)
                 # price
-                i['price'] = float(self.get_value('table.value>tbody>tr>td:nth-child(2)>input',row))
-                s['items'].append(i)
+                i['Price'] = float(self.get_value('table.value>tbody>tr>td:nth-child(2)>input',row))
+                if ('Items' in s ) : s['Items'].append(i)
         # 見積もり合計値のチェック
         self.check_estimate(estimate)
         return estimate
 
     def check_estimate(self, estimate):
         # subTotalの合計が月額合計と等しいか
-        assert round(estimate['total'],2)== round(sum([ a['subTotal'] for a in estimate['detail'] ] ),2)
+        assert round(estimate['Total'],2)== round(sum([ a['SubTotal'] for a in estimate['Detail'] ] ),2)
 
         #各subTotalが項目合計と等しいか
-        for field in estimate['detail']:
-            print field['name'], field['subTotal']  
-            assert round(field['subTotal'],2) == round(sum([ a['price']  for a in field['items'] ]),2)
+        for field in estimate['Detail']:
+            #print field['Name'], field['SubTotal']  
+            assert round(field['SubTotal'],2) == round(sum([ a['Price']  for a in field['Items'] ]),2)
                 
 
     def disable_freetier(self):
